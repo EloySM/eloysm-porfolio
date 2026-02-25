9d037dedfb9d6ba3d6d9d631be8669078312ae96
Eloy <esanchezmeijide@gmail.com>
2026-02-25 19:02:58 +0100

    Añadido de portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [{
+      provider: fontProviders.fontsource(),
+      name: 'Onest',
+      cssVariable: '--font-onest'
+      },{
+        provider: fontProviders.fontsource(),
+        name: 'Cal Sans',
+        cssVariable: '--font-cal-sans'
+      }
+    ]
+  },
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
